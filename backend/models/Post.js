48f3efc00412664c85c9afd30cc152e4ae05d12a
byeongsu364 <kbsyjysbs@gmail.com>
2025-08-13 11:44:27 +0900

    프론트 연결

---
+++ backend/models/Post.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose")
+
+const postSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            require: true,
+            trim: true
+        },
+        content: {
+            type: String,
+            require: true
+        },
+        auther: {
+            type: String,
+            default: "익명"
+        },
+    },
+    {
+        timestamps: true
+    }
+
+
+)
+
+module.exports = mongoose.model("Post", postSchema)
